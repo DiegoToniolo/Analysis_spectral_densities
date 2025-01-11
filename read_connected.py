773de496f47eb2d04a6726797ead985d40f8c5d5
Diego Toniolo <diegotoniolo13@gmail.com>
2025-01-11 17:01:48 +0100

    implementation of reading of paths in read_connected.py and read_connected.in

--- read_connected.py
+++ read_connected.py
@@ -6,26 +6,32 @@ import numpy as np
 class Input_file:
     def __init__(self, path):
         self.file_path = path
-        self.in_file = open(self.file_path, "r")
-        self.runs = self.read_setting("runs", "float64")
+        self.corr_runs_path = self.read_setting("corr_runs_path")
+        self.weight_runs_path = self.read_setting("weight_runs_path")
+        self.corr_runs = self.read_setting("corr_runs")
+        self.weight_runs = self.read_setting("weight_runs")
     
-    def read_setting(self, opt_name, dtype):
+    def read_setting(self, opt_name, dtype='str'):
+        self.in_file = open(self.file_path, "r")
         lines = self.in_file.readlines()
-        
         flag = 0
         for l in lines:
-            tokens = l.split(" ")
+            tokens = l.split()
             if tokens[0] == opt_name:
                 del tokens[0]
                 tokens = np.array(tokens, dtype)
                 flag = 1
-            break
+                break
+        
+        self.in_file.close()
 
         if flag == 0:
-            print("Option" + opt_name + "not found")
+            print("Option " + opt_name + " not found")
             exit(1)
-        
-        return tokens
+        if len(tokens) == 1:
+            return tokens[0]
+        else:
+            return tokens
  
 #Exectution
 if(len(sys.argv) < 2):
@@ -34,5 +40,7 @@ if(len(sys.argv) < 2):
 
 in_f = Input_file(sys.argv[1])
 
-print(in_f.file_path)
-print(in_f.runs)
+print(in_f.corr_runs_path)
+print(in_f.weight_runs_path)
+print(in_f.corr_runs)
+print(in_f.weight_runs)
