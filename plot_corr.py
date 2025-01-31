3b8566cc85a4a782d27de5a4ef3cdc5e6999789a
868635 Diego Toniolo <dtoniolo@knuth.mib.infn.it>
2025-01-31 17:03:17 +0100

    In plot_corr, implementation of plots of correlators and of error ratio between standard and multilevel

--- plot_corr.py
+++ plot_corr.py
@@ -1,6 +1,5 @@
 import numpy as np
 import sys
-import os
 import subprocess
 import re
 from matplotlib import pyplot as plt
@@ -10,6 +9,16 @@ class Input_file:
         if path != "":
             self.file_path = path
             self.in_dir = self.read_setting("in_dir")[0]
+            self.out_dir = self.read_setting("out_dir")[0]
+            s = self.read_setting("err_ratios")
+            self.err_ratios = []
+            for i in range(len(s)):
+                self.err_ratios.append(s[i].split("-"))
+            self.err_ratios = np.array(self.err_ratios)
+            self.exp_perf = self.read_setting("exp_perf", 'float')
+            if len(self.exp_perf) != len(self.err_ratios[:, 0]):
+                print("Number of expected performance different from number of ratios")
+                exit(1)
     
     def read_setting(self, opt_name: str, dtype='str'):
         self.in_file = open(self.file_path, "r")
@@ -29,10 +38,8 @@ class Input_file:
         if flag == 0:
             print("Option " + opt_name + " not found")
             exit(1)
-        if len(tokens) == 1 and dtype != 'str':
-            return tokens[0]
-        else:
-            return tokens
+        
+        return tokens
 
 #Execution
 class main:
@@ -41,8 +48,50 @@ class main:
             print("Usage: python3 " + sys.argv[0] + " input_file.in")
             exit(1)
         in_f = Input_file(sys.argv[1])
-        dat = self.data_to_read(in_f)
-    
+        dat_files = self.data_to_read(in_f)
+        for df in dat_files:
+            av = np.zeros(0)
+            err = np.zeros(0)
+
+            f = open(in_f.in_dir + df, "r")
+            lines = f.readlines()
+            for l in lines:
+                av = np.append(av, float(l.split()[0]))
+                err = np.append(err, float(l.split()[1]))
+            plt.errorbar(np.array(range(len(av))), av, err, fmt="o", markersize=4)
+            plt.grid(True)
+            plt.xlabel("t/a")
+            plt.ylabel(r"$C(t)$")
+            plt.title("Correlator of the vector current")
+            plt.semilogy()
+            plt.savefig(in_f.out_dir + df.split(".")[0] + ".png")
+            plt.close()
+
+        for i in range(len(in_f.err_ratios)):
+            err = np.zeros(0)
+            f1 = open(in_f.in_dir + in_f.err_ratios[i, 0])
+            f2 = open(in_f.in_dir + in_f.err_ratios[i, 1])
+            l1 = f1.readlines()
+            l2 = f2.readlines()
+            if len(l1) != len(l2):
+                print("Data of different length in err_ratios block {}".format(i+1))
+                exit(1)
+            for l in range(len(l1)):
+                err = np.append(err, float((l1[l].split())[1]) ** 2.0/float((l2[l].split())[1]) ** 2.0)
+            plt.plot(np.array(range(len(err))), err, "o", markersize=4, label="Error ratio")
+            x_grid = np.linspace(0, len(err), 1000)
+            y_grid = np.zeros(len(x_grid))
+            y_grid.fill(in_f.exp_perf[i])
+            plt.grid()
+            plt.xlabel("t/a")
+            plt.ylabel(r"$\sigma_{\mathrm{std}}^2/\sigma_{\mathrm{ml}}^2$")
+            plt.plot(x_grid, y_grid, "black", linestyle="--", linewidth=3, label="Expected performance")
+            plt.legend()
+            plt.title("Variance ratio between Standard and Multilevel averages")
+            plt.savefig(in_f.out_dir + "err_ratio_" + in_f.err_ratios[i, 0].split(".")[0] + "_" + in_f.err_ratios[i, 1].split(".")[0])
+            plt.close()
+
+
     def data_to_read(self, in_f:Input_file = Input_file("")) -> list:
         cmd_cd = ['ls', in_f.in_dir]
         proc = subprocess.Popen(cmd_cd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
